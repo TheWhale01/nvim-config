029cd9eeda0eb12445fb900ed831e65b40b5f1e3
TheWhale01 <ard.rasp01@gmail.com>
2024-04-01 19:13:47 +0200

    added relative line numbers

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.wo.number = true
 vim.opt.colorcolumn = "80"
+vim.wo.relativenumber = true
 
 require('thewhale.lazy')
 require('thewhale.configs')
